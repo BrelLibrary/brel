29e62adf017cf950d58812ac9d4dc722435269df
Robin Schmidiger <rschmidiger64@gmail.com>
2024-02-27 16:43:46 +0100

    changed name from footnote to brel_footnote in init file

--- brel/resource/__init__.py
+++ brel/resource/__init__.py
@@ -3,4 +3,4 @@ from .i_resource import IResource
 from .brel_label import BrelLabel
 
 from .brel_reference import BrelReference
-from .footnote import BrelFootnote
+from .brel_footnote import BrelFootnote
