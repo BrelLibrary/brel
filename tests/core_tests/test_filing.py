a645192546e34379b995d37366896f7a45bf2eee
shreymittal1000 <shreymittal1000@gmail.com>
2025-01-19 18:54:36 +0100

    Brel filing 113 open fix n2

--- tests/core_tests/test_filing.py
+++ tests/core_tests/test_filing.py
@@ -108,7 +108,7 @@ def test_filing_open():
 
     # try to pass all files in the ete folder to the open method
     ete_files = os.listdir("tests/end_to_end_tests/ete_filing")
-    ete_files = [os.path.join("tests/end_to_end_tests/ete_filing", file) for file in ete_files if file.endswith(".xml")]
+    #ete_files = [os.path.join("tests/end_to_end_tests/ete_filing", file) for file in ete_files if file.endswith(".xml")]
     try:
         filing = brel.Filing.open(path=ete_files, mode="xml")
         assert len(filing.get_errors()) == 0, f"Expected no errors, got {filing.get_errors()}"
