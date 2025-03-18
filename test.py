2ab4fc7551c92e6e73ca370560b75d995f55d0a0
Robin Schmidiger <rschmidiger64@gmail.com>
2025-03-19 00:23:45 +0100

    Update requirements, delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import brel
-import brel.utils
-
-filing = brel.utils.open_edgar("0000789019", "10-Q")
-# filing = brel.Filing.open("../nvda/nvda-20220130.html", mode="xhtml")
-
-# print(filing.get_all_facts())
-# print(filing.get_errors())
